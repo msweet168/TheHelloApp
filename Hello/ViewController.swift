083714503ddbf580d6e720cc5e35f2ac12a202a6
msweet168 <mitchellsweet168@aol.com>
2016-10-19 23:14:12 -0400

    First commit: Added code that pulls data from the hello txt file, then puts it into an array. Created a function which scuffles the array to get a different "hello statement" each time its called. Then had the function save the scuffled array to UserDefaults. Added label to storyboard and created launch screen.

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,16 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let helloString = try! String(contentsOfFile:
+        Bundle.main.path(forResource: "Hello", ofType: "txt")!)
+    var helloArray = [String]()
+    var theArray = [String]()
+    
+    @IBOutlet var theLabel:UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        helloArray = helloString.components(separatedBy: "\n")
+        helloArray.remove(at: 0)
+        
+        initArray()
+        
     }
+    
+    
+    func initArray() {
+        
+        if let savedArray = UserDefaults.standard.array(forKey: "array1") {
+            print("Not first launch, pulling array...")
+            
+            theArray = savedArray as! [String]
+            
+        }
+        else {
+            print("First launch, shuffleing array...")
+            
+            var helloShuffle = [String]()
+            
+            while helloArray.count > 0 {
+                let randomIndex = arc4random_uniform(UInt32(helloArray.count))
+                helloShuffle.append(helloArray.remove(at: Int(randomIndex)))
+                
+            }
+            
+            UserDefaults.standard.set(helloShuffle, forKey: "array1")
+            theArray = helloShuffle
+            
+        }
+        
+        
+        showHello()
+        
+    }
+    
+    func showHello() {
+        
+        theLabel.text = theArray[0]
+        theArray.remove(at: 0)
+        UserDefaults.standard.set(theArray, forKey: "array1")
+        
+        
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
 
 
 }
